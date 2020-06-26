2dd9f6e312142cbfa35b3e844c18a4af7cbda5cc
rheynoapria <rheynoapria11@gmail.com>
2020-06-26 12:51:59 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import zipfile
+from flask import Flask, request, redirect, url_for, flash, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = "/Users/ens/Desktop/Upload_job"
+ALLOWED_EXTENSIONS = set(['zip'])
+
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            zip_ref = zipfile.ZipFile(os.path.join(UPLOAD_FOLDER, filename), 'r')
+            zip_ref.extractall(UPLOAD_FOLDER)
+            zip_ref.close()
+            flash('Upload Successfuly')
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return render_template('home.html')
+
+
+# @app.route('/admin')
+# def admin_page():
+#     return render_template('admin.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
